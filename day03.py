fe4d274a0d2a4c51bcba9b6c627e9b1d85a0461c
scottdharris <scottdharris11@yahoo.com>
2025-12-03 08:45:59 -0500

    day 3, part 2

--- day03.py
+++ day03.py
@@ -7,29 +7,30 @@ def solve_part1(lines: list) -> int:
     """part 1 solving function"""
     jolts = 0
     for line in lines:
-        jolts += max_joltage(line)
+        jolts += max_joltage(line, 0, 2, "")
     return jolts
 
 @runner("Day 3", "Part 2")
 def solve_part2(lines: list) -> int:
     """part 2 solving function"""
-    return 0
+    jolts = 0
+    for line in lines:
+        jolts += max_joltage(line, 0, 12, "")
+    return jolts
 
-def max_joltage(batts: str) -> int:
+def max_joltage(batts: str, sidx: int, left: int, jolts: str) -> int:
     """determine max joltage for battery bank"""
-    fidx = 0
-    fval = 0
-    for i, batt in enumerate(batts[:-1]):
+    if left == 0:
+        return int(jolts)
+    midx = 0
+    mval = 0
+    for i, batt in enumerate(batts[sidx:len(batts)-left+1]):
         j = int(batt)
-        if j > fval:
-            fval = j
-            fidx = i
-    sval = 0
-    for batt in batts[fidx+1:]:
-        j = int(batt)
-        if j > sval:
-            sval = j
-    return (fval * 10) + sval
+        if j > mval:
+            mval = j
+            midx = i + sidx
+    jolts += batts[midx]
+    return max_joltage(batts, midx+1, left-1, jolts)
 
 # Data
 data = read_lines("input/day03/input.txt")
@@ -43,5 +44,5 @@ assert solve_part1(sample) == 357
 assert solve_part1(data) == 16993
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample) == 3121910778619
+assert solve_part2(data) == 168617068915447
