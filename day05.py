4616a0c0d80c2f5872f8ca983f2697f71566bee9
scottdharris <scottdharris11@yahoo.com>
2025-12-05 11:05:51 -0500

    day 5, part 2

--- day05.py
+++ day05.py
@@ -17,7 +17,45 @@ def solve_part1(lines: list) -> int:
 @runner("Day 5", "Part 2")
 def solve_part2(lines: list) -> int:
     """part 2 solving function"""
-    return 0
+    fresh_ranges, _ = parse_input(lines)
+
+    # adjust ranges so no overlaps/duplicates
+    while True:
+        work = []
+        changed = False
+        for r in fresh_ranges:
+            overlapped = False
+            for i, wr in enumerate(work[:]):
+                if r[0] >= wr[0] and r[1] <= wr[1]:
+                    # skip, range already covered
+                    overlapped = True
+                    changed = True
+                elif r[0] <= wr[0] and r[1] >= wr[1]:
+                    # new range covers all existing, just extend existing
+                    work[i] = r
+                    overlapped = True
+                    changed = True
+                elif r[0] < wr[0] and r[1] >= wr[0] and r[1] <= wr[1]:
+                    # partial overlap on low side, extend just low side
+                    work[i] = (r[0], wr[1])
+                    overlapped = True
+                    changed = True
+                elif r[0] >= wr[0] and r[0] <= wr[1] and r[1] > wr[1]:
+                    # partial overlap on high side, extend just high side
+                    work[i] = (wr[0], r[1])
+                    overlapped = True
+                    changed = True
+            if not overlapped:
+                work.append(r)
+        fresh_ranges = work
+        if not changed:
+            break
+
+    # total up possible
+    possible_fresh = 0
+    for r in fresh_ranges:
+        possible_fresh += r[1] - r[0] + 1
+    return possible_fresh
 
 def parse_input(lines: list) -> tuple[list,list]:
     """parse input into workable structures"""
@@ -51,5 +89,5 @@ assert solve_part1(sample) == 3
 assert solve_part1(data) == 707
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample) == 14
+assert solve_part2(data) == 361615643045059
