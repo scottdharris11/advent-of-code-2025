58aefabb53d2d29587daf2a259e105f92900395c
scottdharris <scottdharris11@yahoo.com>
2025-12-07 11:15:30 -0600

    day 7, part 1

---
+++ day07.py
@@ -0,0 +1,54 @@
+"""utility imports"""
+from utilities.data import read_lines
+from utilities.runner import runner
+
+@runner("Day 7", "Part 1")
+def solve_part1(lines: list) -> int:
+    """part 1 solving function"""
+    start_x = lines[0].index("S")
+    beams = set()
+    beams.add(start_x)
+    splits = 0
+    for y in range(2,len(lines)):
+        nbeams = set()
+        for beam in beams:
+            if lines[y][beam] == '^':
+                splits += 1
+                nbeams.add(beam-1)
+                nbeams.add(beam+1)
+            else:
+                nbeams.add(beam)
+        beams = nbeams
+    return splits
+
+@runner("Day 7", "Part 2")
+def solve_part2(lines: list) -> int:
+    """part 2 solving function"""
+    return 0
+
+# Data
+data = read_lines("input/day07/input.txt")
+sample = """.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............""".splitlines()
+
+# Part 1
+assert solve_part1(sample) == 21
+assert solve_part1(data) == 1499
+
+# Part 2
+assert solve_part2(sample) == 0
+assert solve_part2(data) == 0
