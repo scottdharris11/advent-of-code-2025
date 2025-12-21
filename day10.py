55c26389a13965df099bc78795e2c0e2ebcab2cf
scottdharris <scottdharris11@yahoo.com>
2025-12-21 14:42:56 -0500

    day 10, part 1 speed up

--- day10.py
+++ day10.py
@@ -8,7 +8,9 @@ def solve_part1(lines: list) -> int:
     min_presses = 0
     for _, line in enumerate(lines):
         m = Machine(line)
-        mip = min_init_presses(m, [(None, [0]*m.light_count)], 0)
+        istates = set()
+        istates.add(m.default_state())
+        mip = min_init_presses(m, istates, 0, set())
         min_presses += mip
         #print(f"minimum presses for machine {i+1} of {len(lines)}: {mip}")
     return min_presses
@@ -22,19 +24,20 @@ class Machine:
     """structure represents a machine"""
     def __init__(self, line: str):
         splits = line.split()
-        self.init_pattern = []
+        init_pattern = []
         for s in splits[0][1:-1]:
             if s == '#':
-                self.init_pattern.append(1)
+                init_pattern.append(1)
             else:
-                self.init_pattern.append(0)
+                init_pattern.append(0)
+        self.init_pattern = tuple(init_pattern)
         self.light_count = len(self.init_pattern)
         self.buttons = []
         for s in splits[1:-1]:
             button = []
             for b in s[1:-1].split(','):
                 button.append(int(b))
-            self.buttons.append(button)
+            self.buttons.append(tuple(button))
         self.joltage = []
         for j in splits[-1][1:-1].split(','):
             self.joltage.append(int(j))
@@ -42,29 +45,35 @@ class Machine:
     def __repr__(self):
         return str((self.light_count,self.init_pattern, self.buttons, self.joltage))
 
-    def light_state(self, current: list[int], button: list[int]) -> list[int]:
+    def default_state(self) -> tuple[int]:
+        """default light state for the machine"""
+        state = [0] * self.light_count
+        return tuple(state)
+
+    def light_state(self, state: tuple[int], button: tuple[int]) -> tuple[int]:
         """light state after button applied"""
-        output = current[:]
+        output = list(state)
         for idx in button:
             output[idx] = (output[idx] + 1) % 2
-        return output
+        return tuple(output)
 
     def goal_state(self, state) -> bool:
         """determine if supplied state is the initialization state"""
         return state == self.init_pattern
 
-def min_init_presses(m: Machine, states: list, presses: int) -> int:
+def min_init_presses(m: Machine, states: set, presses: int, seen: set) -> int:
     """minimum number of button presses to reach machine initializaation state"""
-    next_states = []
-    for last_button, state in states:
+    next_states = set()
+    for state in states:
         for button in m.buttons:
-            if last_button == button:
-                continue
             next_state = m.light_state(state, button)
             if m.goal_state(next_state):
                 return presses + 1
-            next_states.append((button,next_state))
-    return min_init_presses(m, next_states, presses+1)
+            if next_state in seen:
+                continue
+            next_states.add(next_state)
+            seen.add(next_state)
+    return min_init_presses(m, next_states, presses+1, seen)
 
 # Data
 data = read_lines("input/day10/input.txt")
