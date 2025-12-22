37c7d5d63c57b4a2b2424aeb80c18f051bf2740a
scottdharris <scottdharris11@yahoo.com>
2025-12-22 07:55:36 -0500

    day 11 setup

---
+++ day11.py
@@ -0,0 +1,34 @@
+"""utility imports"""
+from utilities.data import read_lines
+from utilities.runner import runner
+
+@runner("Day 11", "Part 1")
+def solve_part1(lines: list) -> int:
+    """part 1 solving function"""
+    return 0
+
+@runner("Day 11", "Part 2")
+def solve_part2(lines: list) -> int:
+    """part 2 solving function"""
+    return 0
+
+# Data
+data = read_lines("input/day11/input.txt")
+sample = """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out""".splitlines()
+
+# Part 1
+assert solve_part1(sample) == 5
+assert solve_part1(data) == 0
+
+# Part 2
+assert solve_part2(sample) == 0
+assert solve_part2(data) == 0
