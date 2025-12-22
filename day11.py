edcb83b6f9760a86cfad530828757be7ef4106fa
scottdharris <scottdharris11@yahoo.com>
2025-12-22 16:04:25 -0500

    day 11, part 2

--- day11.py
+++ day11.py
@@ -7,22 +7,52 @@ def solve_part1(lines: list) -> int:
     """part 1 solving function"""
     devices = parse_devices(lines)
     paths = []
-    record_paths(devices, (), "you", paths)
+    record_paths(devices, (), "you", "out", paths)
     return len(paths)
 
 @runner("Day 11", "Part 2")
 def solve_part2(lines: list) -> int:
     """part 2 solving function"""
-    return 0
+    devices = parse_devices(lines)
+    _, _, _, both = count_paths_to(devices, "svr", "out", {})
+    return both
 
-def record_paths(devices: dict, path: tuple, d: str, paths: list):
+def record_paths(devices: dict, path: tuple, d: str, goal: str, paths: list):
     """recursively find paths to output"""
     p = (*path, d)
-    if d == "out":
+    if d == goal:
         paths.append(p)
         return
     for n in devices.get(d,[]):
-        record_paths(devices, p, n, paths)
+        if n not in p:
+            record_paths(devices, p, n, goal, paths)
+
+def count_paths_to(devices: dict, device: str, goal: str, cache: dict) -> tuple[int]:
+    """recursively find paths to output"""
+    if device in cache:
+        return cache[device]
+    paths = 0
+    paths_with_fft = 0
+    paths_with_dac = 0
+    paths_with_both = 0
+    for n in devices.get(device,[]):
+        if n == goal:
+            paths += 1
+        else:
+            p, fc, dc, bc = count_paths_to(devices, n, goal, cache)
+            paths += p
+            paths_with_fft += fc
+            paths_with_dac += dc
+            paths_with_both += bc
+            if n == "fft":
+                paths_with_fft += p
+                paths_with_both += dc
+            elif n == "dac":
+                paths_with_dac += p
+                paths_with_both += fc
+    results = (paths, paths_with_fft, paths_with_dac, paths_with_both)
+    cache[device] = results
+    return results
 
 def parse_devices(lines: list) -> dict[str,list[str]]:
     """parse devices from input"""
@@ -43,11 +73,24 @@ fff: out
 ggg: out
 hhh: ccc fff iii
 iii: out""".splitlines()
+sample2 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out""".splitlines()
 
 # Part 1
 assert solve_part1(sample) == 5
 assert solve_part1(data) == 555
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample2) == 2
+assert solve_part2(data) == 502447498690860
