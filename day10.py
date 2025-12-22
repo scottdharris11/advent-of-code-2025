1821260e63722b81a3b387b554d6f200edf06458
scottdharris <scottdharris11@yahoo.com>
2025-12-22 07:50:24 -0500

    day 10, part 2, state space too big

--- day10.py
+++ day10.py
@@ -18,7 +18,15 @@ def solve_part1(lines: list) -> int:
 @runner("Day 10", "Part 2")
 def solve_part2(lines: list) -> int:
     """part 2 solving function"""
-    return 0
+    min_presses = 0
+    for i, line in enumerate(lines):
+        m = Machine(line)
+        istates = set()
+        istates.add(m.default_state())
+        mjp = min_joltage_presses(m, istates, 0, set())
+        min_presses += mjp
+        print(f"minimum presses for machine {i+1} of {len(lines)}: {mjp}")
+    return min_presses
 
 class Machine:
     """structure represents a machine"""
@@ -38,9 +46,10 @@ class Machine:
             for b in s[1:-1].split(','):
                 button.append(int(b))
             self.buttons.append(tuple(button))
-        self.joltage = []
+        joltage = []
         for j in splits[-1][1:-1].split(','):
-            self.joltage.append(int(j))
+            joltage.append(int(j))
+        self.joltage = tuple(joltage)
 
     def __repr__(self):
         return str((self.light_count,self.init_pattern, self.buttons, self.joltage))
@@ -57,9 +66,27 @@ class Machine:
             output[idx] = (output[idx] + 1) % 2
         return tuple(output)
 
-    def goal_state(self, state) -> bool:
+    def goal_init_state(self, state) -> bool:
         """determine if supplied state is the initialization state"""
         return state == self.init_pattern
+
+    def joltage_state(self, state: tuple[int], button: tuple[int]) -> tuple[int]:
+        """light state after button applied"""
+        output = list(state)
+        for idx in button:
+            output[idx] += 1
+        return tuple(output)
+
+    def goal_joltage_state(self, state) -> bool:
+        """determine if supplied state is the ideal joltage state"""
+        return state == self.joltage
+
+    def over_joltage(self, state: tuple[int]) -> bool:
+        """determine if state is above joltage level"""
+        for i, j in enumerate(self.joltage):
+            if state[i] > j:
+                return True
+        return False
 
 def min_init_presses(m: Machine, states: set, presses: int, seen: set) -> int:
     """minimum number of button presses to reach machine initializaation state"""
@@ -67,13 +94,29 @@ def min_init_presses(m: Machine, states: set, presses: int, seen: set) -> int:
     for state in states:
         for button in m.buttons:
             next_state = m.light_state(state, button)
-            if m.goal_state(next_state):
+            if m.goal_init_state(next_state):
                 return presses + 1
             if next_state in seen:
                 continue
             next_states.add(next_state)
             seen.add(next_state)
     return min_init_presses(m, next_states, presses+1, seen)
+
+def min_joltage_presses(m: Machine, states: set, presses: int, seen: set) -> int:
+    """minimum number of button presses to reach machine joltage state"""
+    next_states = set()
+    for state in states:
+        for button in m.buttons:
+            next_state = m.joltage_state(state, button)
+            if m.goal_joltage_state(next_state):
+                return presses + 1
+            if next_state in seen:
+                continue
+            if m.over_joltage(next_state):
+                continue
+            next_states.add(next_state)
+            seen.add(next_state)
+    return min_joltage_presses(m, next_states, presses+1, seen)
 
 # Data
 data = read_lines("input/day10/input.txt")
@@ -86,5 +129,5 @@ assert solve_part1(sample) == 7
 assert solve_part1(data) == 498
 
 # Part 2
-assert solve_part2(sample) == 0
+assert solve_part2(sample) == 33
 assert solve_part2(data) == 0
